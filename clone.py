ed008e083ed1aed0d28430a258271476734f9244
Paul Mack <pjmst37@gmail.com>
2024-09-30 11:03:00 -0400

    Added ability to change defaults through prompts

--- clone.py
+++ clone.py
@@ -6,6 +6,19 @@ default_clone_cmd = 'git clone'
 with open('config.yaml', 'r') as repo_config:
     config = yaml.safe_load(repo_config)
 
-os.system('cd ' + config['working_directory'])
+working_dir = input(f"Working directory set to: {config['working_directory']}, is this correct? [y/N]")
+if (working_dir == 'N'):
+    working_dir = input(f'Please type in working directory: ')
+else:
+    working_dir = config['working_directory']
+
+base_url = input(f"Base URL is set to: {config['base_url']}, is this correct? [y/N]")
+if (base_url == 'N'):
+    base_url = input(f'Please enter base URL for repository: ')
+else:
+    base_url = config['base_url']
+
+os.system('cd ' + working_dir)
 for repo in config['repos']:
-    os.system(f'{default_clone_cmd} {config['base_url']}/{repo}')
+    print(f'Cloning {repo} from {base_url}/{repo} ..')
+    # os.system(f'{default_clone_cmd} {base_url}/{repo}')
